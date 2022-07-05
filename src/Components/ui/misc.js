e759e8481d99aa2ad84a5db1897547ef88d7af73
Chantha22@ <thasmile1@gmail.com>
2022-07-05 13:41:48 +0700

    update 05072022

---
+++ src/Components/ui/misc.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+export const Tag = (props) =>{
+    const template=<div>tag</div>
+
+    if(props.link){
+        return( 
+            <Link to={props.linkto}>
+                {template}
+            </Link>
+        )
+    }else{
+        return template
+    }
+}
